37dc69aa1e1e22431fbd6af124c8f6e6d3bcbe29
Scott Joyner <kipnerter@gmail.com>
2026-05-30 07:54:33 -0400

    Register passive status routes

--- src/assistx/api_router.py
+++ src/assistx/api_router.py
@@ -3,9 +3,11 @@ from __future__ import annotations
 from .api import app, _neo, auth
 from .passive_agents import build_passive_agent_router
 from .passive_claims import build_passive_claim_router
+from .passive_status import build_passive_status_router
 from .router_integration import build_router_integration_router
 
 
 app.include_router(build_router_integration_router(_neo))
 app.include_router(build_passive_agent_router(_neo, auth_dependency=auth))
 app.include_router(build_passive_claim_router(_neo, auth_dependency=auth))
+app.include_router(build_passive_status_router(_neo, auth_dependency=auth))
