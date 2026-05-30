84c2ca7e8a61d7315ab80a06d94680b4ea0e2769
Scott Joyner <kipnerter@gmail.com>
2026-05-30 00:21:06 -0400

    Register passive agent heartbeat routes

--- src/assistx/api_router.py
+++ src/assistx/api_router.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
-from .api import app, _neo
+from .api import app, _neo, auth
+from .passive_agents import build_passive_agent_router
 from .router_integration import build_router_integration_router
 
 
 app.include_router(build_router_integration_router(_neo))
+app.include_router(build_passive_agent_router(_neo, auth_dependency=auth))
